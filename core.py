ce61049404f5c4874cc45fc3c0502c86292f0b72
filip <f.admiakk@gmail.com>
2026-05-11 12:40:43 +0200

    fix: implement CourseGraph registry

--- core.py
+++ core.py
@@ -9,3 +9,24 @@ class Subject:
     def add_dependent(self, next_subject):
         self.dependent_subjects.add(next_subject)
         next_subject.in_degree += 1
+
+class CourseGraph:
+    def __init__(self):
+        self.subjects = {}
+
+    def add_subject(self, subject_obj):
+        if subject_obj.name in self.subjects:
+            raise ValueError(f"Subject : {subject_obj.name} already exists in graph.")
+
+        self.subjects[subject_obj.name] = subject_obj
+
+    def add_dependent(self, prereq_name, target_name):
+        if prereq_name not in self.subjects:
+            raise ValueError(f"Subject : {prereq_name} does not exist in graph.")
+        if target_name not in self.subjects:
+            raise ValueError(f"Subject : {target_name} does not exist in graph.")
+
+        prereq = self.subjects[prereq_name]
+        target = self.subjects[target_name]
+
+        prereq.add_dependent(target)
