c330b9affaf581fd785896ab49a4c437472359f5
filip <f.admiakk@gmail.com>
2026-05-13 17:31:39 +0200

    feat: add docker-compose and configurate env-base database connection

--- database.py
+++ database.py
@@ -1,8 +1,12 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # database URL postgresql://user:password@host:port/database_name
-SQLALCHEMY_DATABASE_URL = "postgresql://admin:secret@localhost:5432/examflow"
+SQLALCHEMY_DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://admin:secret@localhost:5432/examflow"
+)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
