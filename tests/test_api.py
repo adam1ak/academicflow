79a5f22ce2ed9f489214a6ef1da13a0d85454fe2
filip <f.admiakk@gmail.com>
2026-05-13 21:17:29 +0200

    test: add db integration test for algorithm

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,4 +1,9 @@
+import os
+
+os.environ["DATABASE_URL"] = "sqlite:///./test_database.db"
+
 from fastapi.testclient import TestClient
+
 from api import app
 
 client = TestClient(app)
@@ -13,3 +18,38 @@ def test_health_check():
         "status": "ok",
         "message": "AcademicFlow API is running"
     }
+
+
+def test_generate_plan():
+    payload = {
+        "max_concurrent" : 2,
+        "subjects": [
+            {
+                "name": "Calculus1",
+                "field": "Math",
+                "duration": 5,
+                "dependents": ["Calculus2"]
+            },
+            {
+                "name": "Calculus2",
+                "field": "Math",
+                "duration": 2,
+                "dependents": []
+            }
+        ]
+    }
+
+    response = client.post("/api/v1/generate-plan", json=payload)
+
+    assert response.status_code == 200
+    data = response.json()
+
+    assert len(data) == 2
+
+    assert data[0]["name"] == "Calculus1"
+    assert data[0]["start_time"] == 0
+    assert data[0]["end_time"] == 5
+
+    assert data[1]["name"] == "Calculus2"
+    assert data[1]["start_time"] == 5
+    assert data[1]["end_time"] == 7
