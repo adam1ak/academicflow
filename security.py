ba93d3686ee3ff3f3d240872f084c08d45820d65
filip <f.admiakk@gmail.com>
2026-05-14 21:22:16 +0200

    feat: add JWT authentication, login endpoint and secure test route

--- security.py
+++ security.py
@@ -1,4 +1,10 @@
 import bcrypt
+from datetime import datetime, timedelta, timezone
+from jose import jwt
+
+SECRET_KEY = "d01b6d001a46fc5c598d5f6adaf8f3810b46391ff5503ac3eac39bcd4b616ca8"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 def get_password_hash(password: str) -> str:
     pwd_bytes = password.encode('utf-8')
@@ -10,3 +16,15 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
     password_byte_enc = plain_password.encode('utf-8')
     hashed_password_bytes = hashed_password.encode('utf-8')
     return bcrypt.checkpw(password_byte_enc, hashed_password_bytes)
+
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+
+    to_encode.update({"exp": expire})
+
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
