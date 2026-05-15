11f30b04121bcc7d2f6522af209bae11532dbdf0
filip <f.admiakk@gmail.com>
2026-05-15 16:47:18 +0200

    feat(ui): fetch and display user study plans on dashboard

--- webapp/src/pages/DashboardPage.jsx
+++ webapp/src/pages/DashboardPage.jsx
@@ -1,6 +1,27 @@
 import { logout } from '../services/api'
 
+import { useState, useEffect } from 'react'
+
+import { getMyPlans } from '../services/api'
+
 function DashboardPage({ setIsLogged }) {
+    const [myPlans, setMyPlans] = useState([])
+
+    useEffect(() => {
+        const fetchPlans = async () => {
+            try {
+                const plans = await getMyPlans()
+                console.log(plans)
+
+                setMyPlans(plans)
+            } catch (e) {
+                console.log("Error while fetching plans: ", e);
+            }
+        }
+
+        fetchPlans()
+    }, [])
+
     const handleLogout = () => {
         logout()
         setIsLogged(false)
@@ -11,6 +32,25 @@ function DashboardPage({ setIsLogged }) {
         <div>
             <h1>Login succeed!</h1>
             <button onClick={handleLogout}>Log out</button>
+
+            <h2>Your Study Plans</h2>
+
+            {myPlans.length === 0 ? (
+                <p>No plans generated yet.</p>
+            ) : (
+                myPlans.map((plan) => (
+                    <div key={plan.id} style={{ border: '1px solid black', margin: '10px', padding: '10px' }}>
+                        <h3>{plan.name}</h3>
+                        <ul>
+                            {plan.schedule.map((subject) => (
+                                <li key={subject.name}>
+                                    <strong>{subject.name}</strong> (Week: {subject.start_time} - {subject.end_time})
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                ))
+            )}
         </div>
     )
 }
