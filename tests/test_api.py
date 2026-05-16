4734b7abed19535ab0745a4f17d7dc7b98944db7
filip <f.admiakk@gmail.com>
2026-05-16 22:39:09 +0200

    fix(tests): update expected status code to 201 in generate plan test

--- tests/test_api.py
+++ tests/test_api.py
@@ -18,7 +18,7 @@ def test_health_check():
     response = client.get("/")
 
     # OK
-    assert response.status_code == 200
+    assert response.status_code == 201
 
     assert response.json() == {
         "status": "ok",
