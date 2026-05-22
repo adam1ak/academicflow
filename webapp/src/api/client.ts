4219b4cfb763a1b93d7ac61438492716e9598516
filip <f.admiakk@gmail.com>
2026-05-22 15:51:23 +0200

    fix(db): ensure tables are automatically created on application startup

--- webapp/src/api/client.ts
+++ webapp/src/api/client.ts
@@ -11,7 +11,7 @@ const api = axios.create({
 })
 
 api.interceptors.request.use((config: InternalAxiosRequestConfig) => {
-    const token = tokenStorage.getAccessToken
+    const token = tokenStorage.getAccessToken()
     if (token) {
         config.headers.Authorization = `Bearer ${token}`
     }
