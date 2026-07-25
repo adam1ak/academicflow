6272ad81c263b01fc4825a6bf0701dde0b83bd78
filip <f.admiakk@gmail.com>
2026-07-25 14:09:42 +0200

    fix(db): automatically convert postgres:// schema prefix to postgressql://

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from config import settings
 
-SQLALCHEMY_DATABASE_URL = settings.database_url
+db_url = settings.database_url
+if db_url.startswith("postgres://"):
+    db_url = db_url.replace("postgres://", "postgresql://", 1)
+
+SQLALCHEMY_DATABASE_URL = db_url
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
