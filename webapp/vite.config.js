4a3e48f055cae95cf08eed708dc81aa5857f1306
adam1ak <f.admiakk@gmail.com>
2026-09-10 20:46:08 +0200

    test(webapp): configure Vitest, React Testing Library, MSW and add first component test

--- webapp/vite.config.js
+++ webapp/vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/setupTests.ts'
+    setupFiles: './src/setupTests.ts',
+    include: ['src/**/*.{test,spec}.{ts,tsx}']
   }
 })
